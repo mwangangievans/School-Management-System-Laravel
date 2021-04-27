e97f58c8efdf16d661baade4170c48aee405373c
MuindeMwangangi <muindemwangangi2@gmail.com>
2021-04-27 12:26:25 -0700

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,11 +38,9 @@
             </button>
              </div>
 
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline  "  type="submit">
-                Register
-            </button>
+
              </div>
-        </div>
+
     </form>
 </div>
 
